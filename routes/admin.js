2a6b0ba34e14d47f7d774dfb0729ed1d33cceec2
shimkijun <shimkijun@gmail.com>
2021-03-07 13:09:30 +0900

    end express

---
+++ routes/admin.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+function testMiddleware(req,res,next){
+    console.log('첫번째 미들웨어')
+    next();
+}
+
+router.get('/',testMiddleware,(req,res)=>{
+    res.send('admin next url')
+})
+
+router.get('/products',(req,res)=>{
+    res.render('admin/products.html' , {
+        message : "hi products"
+    })
+})
+
+router.get('/products/write',(req,res)=>{
+    res.render('admin/write.html')
+})
+
+router.post('/products/write',(req,res)=>{
+    res.send(req.body);
+})
+
+module.exports = router
